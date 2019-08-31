463214654774489993bc72d501e623520fe35b0a
jstran9 <wptran58@gmail.com>
2019-08-30 18:21:44 -0700

    Adding new route to display and delete single post. Adding route to process request for searching by hashtags or description/body of a post.

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
+  get 'search/index'
   devise_for :users
   get 'home/index'
+  get 'search' => 'search#index'
 
   resources :users, only: [:show, :edit, :update]
-  resources :posts, only: [:new, :create]
+  resources :posts, only: [:new, :create, :show, :destroy]
 
   root to: 'home#index'
 end
